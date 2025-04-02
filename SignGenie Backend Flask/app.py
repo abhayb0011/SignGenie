c1ff465606107dcfc4dcd54ef0c6df0f222c1964
Abhay Bhardwaj <153390029+abhayb0011@users.noreply.github.com>
2025-04-02 08:06:49 +0530

    Update app.py

--- SignGenie Backend Flask/app.py
+++ SignGenie Backend Flask/app.py
@@ -195,5 +195,8 @@ def get_signs():
     return jsonify(signs)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT env variable or default to 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
